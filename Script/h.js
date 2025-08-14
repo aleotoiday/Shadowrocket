224cd89e9d44106edc0154c3daf74c25d5cbd453
aleotoiday <aleotoiday@gmail.com>
2025-08-14 14:12:29 +0700

    Delete Script/h.js

--- Script/h.js
+++
@@ -1,5 +0,0 @@
-var Q = JSON.parse($response.body);
-Q.data.settings.lifetime_subscription = true;
-Q.data.settings.store_subscription = true;
-Q.data.settings.subscription = true;
-$done({ body: JSON.stringify(Q) });
